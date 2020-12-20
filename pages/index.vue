c6eefe29a8cf16323cf31665ffdb8a541b53df64
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-20 10:59:23 +0330

    narrow -offset-touch(for responsive mobile)

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,67 @@
 <template>
   <div class="container">
-    <h1 class="title">HOME PAGE</h1>
+    <div class="section">
+      <div class="conatiner">
+        <h1 class="title">HOME PAGE</h1>
 
-    <div class="columns">
-      <div class="column is-full">
-        <div class="notification is-link">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.
+        <div class="columns is-mobile">
+          <div class="column is-11 is-offset-1">
+            <div class="notification is-link">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae
+              congue mauris rhoncus aenean vel elit scelerisque. In egestas erat
+              imperdiet sed euismod nisi porta lorem mollis. Morbi tristique
+              senectus et netus. Mattis pellentesque id nibh tortor id aliquet
+              lectus proin. Sapien faucibus et molestie ac feugiat sed lectus
+              vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt
+              ornare massa eget. Dictum varius duis at consectetur lorem. Nisi
+              vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut
+              tortor pretium viverra suspendisse potenti nullam. Et molestie ac
+              feugiat sed lectus. Non nisi est sit amet facilisis magna.
+              Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut
+              enim blandit volutpat maecenas volutpat. Ornare lectus sit amet
+              est placerat in egestas erat. Nisi vitae suscipit tellus mauris a
+              diam maecenas sed. Placerat duis ultricies lacus sed turpis
+              tincidunt id aliquet.
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
 
-    <div class="columns">
-      <div class="column is-7">
-        <div class="notification is-primary">1</div>
-      </div>
-      
-      <div class="column is-2">
-        <div class="notification is-warning">2</div>
-      </div>
-      <div class="column is-3">
-        <div class="notification is-danger">3</div>
-      </div>
+        <div class="columns">
+          <div class="column is-7">
+            <div class="notification is-primary">7</div>
+          </div>
+
+          <div class="column is-2">
+            <div class="notification is-warning">2</div>
+          </div>
+          <div class="column is-3">
+            <div class="notification is-danger">3</div>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="column is-narrow-touch">
+            <div class="box" style="width:200px ">
+              <p class="title">Narrow Columns</p>
+              <p class="subtitle">This Column is on 200px</p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="box">
+              <p class="title">Fixible title columns</p>
+              <p class="subtitle">Hi I'm Programmer Web Devoloper in frontend</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
