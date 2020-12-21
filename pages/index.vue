ead1fded8d7b74066c5d4e2731578b7ae0e24345
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-21 20:47:23 +0330

    media-object

--- pages/index.vue
+++ pages/index.vue
@@ -3,78 +3,39 @@
     <!-- <h1 class="title has-text-centered has-text-info">Home Page</h1>
     <br /><br /> -->
 
-    <nav class="level">
-      <!-- level left -->
-      <div class="level-left">
-        <div class="level-item">
-          <p class="subtitle is-size-5"><strong>123</strong>Posts</p>
-        </div>
-        
-        <div class="level-item">
-          <div class="field has-addons">
-            <p class="control">
-              <input class="input" type="text" placeholder="Find the Post">
-            </p>
-            
-            <p class="control">
-              <button class="button">Search</button>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <!-- level right -->
-      <div class="level-right">
-        <p class="level-item">
-          <strong>All</strong>
-        </p>
-        <p class="level-item">
-          <a href="#">Publish</a>
-        </p>
-        <p class="level-item">
-          <a href="#">Drafts</a>
-        </p>
-        <p class="level-item">
-          <a href="#">Deleted</a>
-        </p>
-        <p class="level-item">
-          <a href="#" class="button is-success">New</a>
-        </p>
-      </div>
-    </nav>
-
     <div class="container">
-      <div class="level is-mobile">
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Posts</p>
-            <p class="title">3.356</p>
-          </div>
-        </div>
 
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Flowing</p>
-            <p class="title">14.320</p>
-          </div>
-        </div>
+      <div class="media">
 
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Flower</p>
-            <p class="title">86k</p>
-          </div>
-        </div>
+      <!-- media left -->
+      <div class="media-left has-background-danger">
+        M Left
+      </div>
 
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Likes</p>
-            <p class="title">7.897</p>
-          </div>
-        </div>
+      <!-- media content -->
+      <div class="media-content has-background-info">
+        M Content
 
+        <div class="media">
+          <div class="media-left has-background-success">
+            M Left Nested
+          </div>
+
+          <div class="media-content has-background-black">
+            M Content Nested
+          </div>
+
+        </div>
 
       </div>
+
+      <!-- media-right -->
+      <div class="media-right has-background-warning">
+        M right
+      </div>
+
+    </div>
+
     </div>
 
   </div>
