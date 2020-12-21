cdd9b2227303b7083bc542bcf710360d613d7308
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-21 09:10:56 +0330

    element/image

--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,33 @@
 <template>
   <div class="container">
+    <h1 class="title has-text-centered has-text-info">Home Page</h1>
+    <br /><br />
+
     <div class="section">
-      <div class="conatiner">
-        <h1 class="title">HOME PAGE</h1>
-
-        <h1 class="title">
-          colors
-        </h1>
-
-        <p class="subtitle has-text-white has-background-black">
-          white
-        </p>
-
-        <p class="subtitle has-text-primary">
-          primary
-        </p>
-
-        <p class="subtitle has-text-light has-background-dark">
-          light
-        </p>
-
-        <p class="subtitle has-text-info">
-          info
-        </p>
-
-        <p class="subtitle has-text-success">
-          success
-        </p>
-
-        <p class="subtitle has-text-danger">
-          danger
-        </p>
-
-        <p class="subtitle has-text-link">
-          link
-        </p>
-
-        <p class="subtitle has-text-warning">
-          warning
-        </p>
-
-        <p class="subtitle has-text-black-bis">
-          black-bis
-        </p>
-
-        <p class="subtitle has-text-black-ter">
-          black-ter
-        </p>
-
-        <p class="subtitle has-text-grey">
-          grey
-        </p>
-
-        <p class="subtitle has-text-grey-light">
-          grey-light
-        </p>
-
-        <p class="subtitle has-text-grey-lighter">
-          grey-lighter
-        </p>
-
-        <p class="subtitle has-text-white-ter">
-          white-ter
-        </p>
-
-        <p class="subtitle has-text-white-bis">
-          white bis
-        </p>
-
-        <p class="subtitle has-text-primary-light has-background-primary-dark">
-          primary light
-        </p>
-
-        <p class="subtitle has-text-link-light has-background-link-dark">
-          link-light
-        </p>
-
-        <p class="subtitle has-text-info-light has-background-info-dark">
-          info-light
-        </p>
-
-        <p class="subtitle has-text-success-light has-background-success-dark">
-          success-light
-        </p>
-
-        <p class="subtitle has-text-warning-light has-background-warning-dark">
-          warning-light
-        </p>
-
-        <p class="subtitle has-text-danger-light has-background-danger-dark">
-          danger-light
-        </p>
+      <div class="container">
+        
+        <div class="columns">
+          <div class="column">
+            <figure class="image is-128x128">
+              <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-fullwidth">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-5by4">
+              <img src="https://i.pinimg.com/originals/aa/7c/82/aa7c829eb316ab1d077a5768188ee622.jpg" alt="">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-16by9">
+              <img src="https://i.pinimg.com/originals/aa/7c/82/aa7c829eb316ab1d077a5768188ee622.jpg" alt="">
+            </figure>
+          </div>
+        </div>
 
       </div>
     </div>
