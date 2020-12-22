5e4da0f9d6327c437ab6ad4c2813e3833b1231d0
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-22 10:52:15 +0330

    textarea

--- pages/index.vue
+++ pages/index.vue
@@ -4,59 +4,16 @@
     <br /><br /> -->
 
     <div class="container">
-
-      <!-- <div class="columns">
-        <div class="column is-half">
-
-          <div class="field">
-            <div class="control">
-              <input type="text" class="input is-danger is-small is-rounded">
-            </div>
-          </div>
-
-
-          <div class="field">
-            <div class="control is-loading">
-              <input type="text" class="input is-success is-focused">
-            </div>
-          </div>
-
-          <div class="field">
-            <div class="control">
-              <input type="text" class="input is-info is-medium is-rounded">
-            </div>
-          </div>
-
-          <div class="field">
-            <div class="control">
-              <input type="text" class="input is-warning is-large is-hovered" value="Text" readonly>
-            </div>
-          </div>
-
-        </div>
-      </div> -->
-
-      <div class="columns">
-        <div class="column is-half">
-
-          <div class="field">
-            <p class="control has-icons-left has-icons-right">
-              <input type="email" class="input" placeholder="Email">
-            </p>
-          </div>
-
-          <div class="field">
-            <div class="control">
-              <input type="password" class="input" placeholder="Password">
-            </div>
-          </div>
-
-        </div>
+      <div class="control is-loading">
+        <textarea
+          name="textarea1"
+          cols="30"
+          rows="5"
+          class="textarea has-fixed-size is-focused"
+          placeholder="e.g hello world"
+        ></textarea>
       </div>
-      
-
     </div>
-
   </div>
 </template>
 
@@ -65,9 +22,4 @@ export default {}
 </script>
 
 <style>
-.center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 </style>
