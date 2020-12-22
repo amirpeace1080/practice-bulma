759c0868db71faeb76bae92b1813bf5d39266990
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-22 13:44:11 +0330

    checkbox && radiobutton

--- pages/index.vue
+++ pages/index.vue
@@ -3,109 +3,36 @@
     <!-- <h1 class="title has-text-centered has-text-info">Home Page</h1>
     <br /><br /> -->
 
+    <!-- <div class="container">
+
+      <label class="checkbox" disabled>
+        <input type="checkbox" disabled>
+        I Agree to <a href="#">term and condition</a>
+      </label>
+
+    </div> -->
+
     <div class="container">
-      <!-- <div class="select is-multiple">
-        <select multiple size="8">
-          <option value="Tehran">Tehran</option>
-          <option value="Esfahan">Esfahan</option>
-          <option value="Shiraz">Shiraz</option>
-          <option value="Mashhad">Mashhad</option>
-          <option value="Tabriz">Tabriz</option>
-        </select>
-      </div> -->
 
-      <div class="field">
-        <div class="control">
-          <div class="select">
-            <select class="is-focused">
-              <option>Tehran</option>
-              <option>Esfahan</option>
-              <option>Shiraz</option>
-              <option>Mashhad</option>
-              <option>Tabriz</option>
-            </select>
-          </div>
-        </div>
+      <div class="control">
+        <label class="radio">
+          <input type="radio" name="answer" checked>
+          Small
+        </label>
+
+        <label class="radio">
+          <input type="radio" name="answer">
+          Medium
+        </label>
+
+        <label class="radio" disabled>
+          <input type="radio" name="answer" disabled>
+          Large
+        </label>
       </div>
-
-      <div class="field">
-        <div class="control">
-          <div class="select">
-            <select class="is-hovered">
-              <option>Tehran</option>
-              <option>Esfahan</option>
-              <option>Shiraz</option>
-              <option>Mashhad</option>
-              <option>Tabriz</option>
-            </select>
-          </div>
-        </div>
-      </div>
-
-      <div class="field">
-        <div class="control">
-          <div class="select">
-            <select class="active">
-              <option>Tehran</option>
-              <option>Esfahan</option>
-              <option>Shiraz</option>
-              <option>Mashhad</option>
-              <option>Tabriz</option>
-            </select>
-          </div>
-        </div>
-      </div>
-
-      <div class="field">
-        <div class="control">
-          <div class="select is-success is-large is-rounded">
-            <select>
-              <option>Tehran</option>
-              <option>Esfahan</option>
-              <option>Shiraz</option>
-              <option>Mashhad</option>
-              <option>Tabriz</option>
-            </select>
-          </div>
-        </div>
-      </div>
-
-      <div class="field">
-        <div class="control">
-          <div class="select is-warning is-loading">
-            <select>
-              <option>Tehran</option>
-              <option>Esfahan</option>
-              <option>Shiraz</option>
-              <option>Mashhad</option>
-              <option>Tabriz</option>
-            </select>
-          </div>
-        </div>
-      </div>
-
-      <div class="field">
-        <div class="control has-icons-left">
-          <div class="select">
-            <select>
-              <option>Tehran</option>
-              <option>Esfahan</option>
-              <option>Shiraz</option>
-              <option>Mashhad</option>
-              <option>Tabriz</option>
-            </select>
-          </div>
-
-          <div class="icon is-small is-left">
-            <i class="fa fa-car"></i>
-          </div>
-
-        </div>
-      </div>
-
-
 
     </div>
+    
   </div>
 </template>
 
