ed62093ea389cd64e14fdfee0db4f81b9915f80d
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-22 16:05:05 +0330

    create: page-login

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,76 @@
 <template>
-  <div>
+  <div class="home">
     <!-- <h1 class="title has-text-centered has-text-info">Home Page</h1>
     <br /><br /> -->
 
-    <div class="container">
-      <div class="file has-name is-right">
-        <label class="file-label">
-          <input class="file-input" type="file" name="resume">
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        
+        <div class="container">
+          <div class="columns is-centered">
 
-          <span class="file-cta">
-            <span class="file-icon">
-              <i class="fa fa-upload"></i>
-            </span>
-            
-            <span class="file-label">Choose File ....</span>
-          </span>
+            <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+              <form class="box">
 
-          <span class="file-name">
-            Screenshoot-2020-12-18.jpg
-          </span>
-        </label>
+                <div class="field has-text-centered">
+                  <img src="images/logo-bis.png" width="167">
+                </div>
+
+                <div class="field">
+                  <label class="label">ایمیل</label>
+                  <div class="control has-icons-right">
+                    <input type="email" class="input" placeholder="ایمیل را وارد نمایید" required>
+                    <span class="icon is-small is-right">
+                      <i class="fa fa-user"></i>
+                    </span>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label class="label">رمز عبور</label>
+                  <div class="control has-icons-right">
+                    <input type="password" class="input" placeholder="رمز عبور را وارد نمایید" required>
+                    <span class="icon is-small is-right">
+                      <i class="fa fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label class="checkbox">
+                    <input type="checkbox">
+                    مرا به خاطر سپار
+                  </label>
+                </div>
+
+                <div class="field">
+                  <button class="button is-success">ورود</button>
+                </div>
+
+              </form>
+            </div>
+
+          </div>
+        </div>
+
       </div>
-    </div>
+    </section>
     
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  head(){
+    return{
+      title: 'ورود کاربران'
+    }
+  }
+}
 </script>
 
 <style>
+.home{
+  direction: rtl;
+}
 </style>
