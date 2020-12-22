a4ab71e72e45383ce136b4184711fde94a3c35c8
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-22 21:03:58 +0330

    toolbar & create page-book

--- pages/index.vue
+++ pages/index.vue
@@ -69,9 +69,58 @@
     <section class="section">
       <div class="columns">
 
-        <div class="column">
+        <div class="column" style="direction:rtl">
+          <h1 class="title">کتاب ها</h1>
+          <nav class="level">
+            
+            <div class="level-left">
 
+              <div class="level-item">
+                <p class="subtitle">
+                  تعداد <strong>6</strong>
+                </p>
+              </div>
+
+                <p class="level-item">
+                  <a href="#" class="button is-success">جدید</a>
+                </p>
+
+                <div class="level-item">
+                  <div class="field has-addons">
+
+                    <p class="control">
+                      <input type="text" class="input" placeholder="نام کتاب ، ISBN">
+                    </p>
+
+                    <p class="control">
+                      <button class="button">جستجو</button>
+                    </p>
+
+                  </div>
+                </div>
+
+            </div>
+
+
+            <div class="level-right">
+              <div class="level-item">
+                سفارشات براساس
+
+                <div class="level-item">
+                  <div class="select">
+                    <select>
+                      <option>تاریخ انتشار</option>
+                      <option>قیمت</option>
+                      <option>تعداد صفحه</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </nav>
         </div>
+
 
 
         <div class="column is-4-tablet is-3-desktop is-2-widescreen">
@@ -82,16 +131,16 @@
             <ul class="menu-list">
 
               <li>
-                <a href="#">
+                <nuxt-link to="/dashboard">
                   <span class="icon">
                     <i class="fa fa-digital-tachograph"></i>
                   </span>
                   داشبورد
-                </a>
+                </nuxt-link>
               </li>
 
               <li>
-                <a href="#">
+                <a href="/book" class="is-active">
                   <span class="icon">
                     <i class="fa fa-book"></i>
                   </span>
@@ -100,21 +149,21 @@
               </li>
 
               <li>
-                <a href="#">
+                <nuxt-link to="/customers">
                   <span class="icon">
                     <i class="fa fa-address-book"></i>
                   </span>
                   مشتری ها
-                </a>
+                </nuxt-link>
               </li>
 
               <li>
-                <a href="#">
+                <nuxt-link to="/orders">
                   <span class="icon">
                     <i class="fa fa-file"></i>
                   </span>
                   سفارشات
-                </a>
+                </nuxt-link>
               </li>
 
 
